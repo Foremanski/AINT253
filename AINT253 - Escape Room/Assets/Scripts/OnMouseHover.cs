055a1703e6df149bf1bea14ed080a7caeafa0052
Foremanski <daniel.foreman@students.plymouth.ac.uk>
2019-11-19 16:39:05 +0000

    Animation and Interaction
    Added UI that appears when looking at interactable objects. Also added animation for gate. Writing scripts to play animation when the player hovers over these interactable objects

---
+++ AINT253 - Escape Room/Assets/Scripts/OnMouseHover.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayOnMouseHover : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string myString;
+    public Text myText;
+    public float fadeTime;
+    public bool displayInfo;
+
+    void Start()
+    {
+        myText = GameObject.Find("Text").GetComponent<Text>();
+
+
+        myText.color = Color.clear;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FadeText();        
+    }
+    void OnMouseOver()
+    {
+        displayInfo = true;
+    }
+
+    void OnMouseExit()
+    {
+        displayInfo = false;
+    }
+
+    void FadeText()
+    {
+        if (displayInfo)
+        {
+            myText.text = myString;
+            myText.color = Color.Lerp(myText.color, Color.black, fadeTime * Time.deltaTime);
+        }
+
+        else
+        {
+            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+    }
+}
+ 
---
+++ AINT253 - Escape Room/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public Animator animatedObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatedObject.GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown("E"))
+        {
+            animatedObject.Play("GateOpen");
+        }
+    }
+}
