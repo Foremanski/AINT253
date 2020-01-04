a2d0a9546a985c385cbf49587a5cf845e09e4de1
Foremanski <daniel.foreman@students.plymouth.ac.uk>
2020-01-04 11:23:12 +0000

    Record Player
    Modelled a record player for my escape room

--- AINT253 - Escape Room/Assets/Scripts/inventoryManager.cs
+++ AINT253 - Escape Room/Assets/Scripts/inventoryManager.cs
@@ -16,7 +16,7 @@ public class inventoryManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        hasDisc = true;
     }
 
     // Update is called once per frame
