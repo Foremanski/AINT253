b3b19fa47e32cf5f48073b5aaeb8430f504d21e6
Foremanski <daniel.foreman@students.plymouth.ac.uk>
2020-01-05 20:07:57 +0000

    Wallpaper and lightmaps
    Messed around with lighting, added wallpaper, brick and concrete to my scene (will change)

---
+++ AINT253 - Escape Room/Assets/Scripts/lightFlicker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightFlicker : MonoBehaviour
+{ 
+
+    public GameObject flickerLight;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        StartCoroutine(flicker());
+    }
+
+    // Update is called once per frame
+    IEnumerator flicker()
+    {
+        bool sceneactive = true;
+        float waitTime = Random.Range(5.0f, 1000.0f);
+
+        while (sceneactive == true)
+        {
+            
+            flickerLight.SetActive(true);
+            yield return new WaitForSeconds(waitTime);
+            flickerLight.SetActive(false);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
